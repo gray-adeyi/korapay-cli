17bda9bc18b2722b7040dc7073fa6d7c492cb1eb
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-02-08 18:24:09 +0100

    Implement cli entry point

--- src/korapay_cli/__init__.py
+++ src/korapay_cli/__init__.py
@@ -1,2 +1,12 @@
-def main() -> None:
-    print("Hello from korapay-cli!")
+from korapay_cli.korapay import root_app
+from korapay_cli.config import config_app
+
+root_app.add_typer(config_app, name="config")
+
+
+def run():
+    root_app()
+
+
+if __name__ == "__main__":
+    run()
